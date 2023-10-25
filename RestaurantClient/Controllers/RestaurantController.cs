48b4939b77dc60fea5b249a354e975eb170d4371
Filip <filipkwiek888@gmail.com>
2023-10-25 20:34:05 +0200

    Szymon śmierdzi

--- RestaurantClient/Controllers/RestaurantController.cs
+++ RestaurantClient/Controllers/RestaurantController.cs
@@ -1,3 +1,4 @@
+using AspNetCoreHero.ToastNotification.Abstractions;
 using DefaultNamespace;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -8,46 +9,49 @@ namespace RestaurantClient.Controllers;
 public class RestaurantController : Controller
 {
     private readonly IRestaurantService _restaurantService;
+    private readonly INotyfService _notyf;
 
-    public RestaurantController(IRestaurantService restaurantService)
+    public RestaurantController(IRestaurantService restaurantService, INotyfService notyf)
     {
         _restaurantService = restaurantService;
+        _notyf = notyf;
     }
     // GET
     public async Task<IActionResult> Index()
     {
         var msg = await _restaurantService.GetRestaurants();
 
-        if (!msg.IsSuccessStatusCode)
+        if (msg.IsSuccessStatusCode)
         {
-            return NotFound();
+            return View(JsonConvert.DeserializeObject<List<Restaurant>>(await msg.Content.ReadAsStringAsync()));
         }
-        
-        return View(JsonConvert.DeserializeObject<List<Restaurant>>(await msg.Content.ReadAsStringAsync()));
+        return NotFound();
     }
 
     [HttpGet("{id}")]
     public async Task<IActionResult> Details(int Id)
     {
         var msg = await _restaurantService.GetRestaurant(Id);
-        if (!msg.IsSuccessStatusCode)
+        
+        if (msg.IsSuccessStatusCode)
         {
-            return NotFound();
+            return View(JsonConvert.DeserializeObject<Restaurant>(await msg.Content.ReadAsStringAsync()));
         }
         
-        return View(JsonConvert.DeserializeObject<Restaurant>(await msg.Content.ReadAsStringAsync()));
+        return NotFound();
     }
 
     [HttpPost]
     public async Task<IActionResult> Add([FromBody]CreateRestaurant dto)
     {
         var msg = await _restaurantService.AddRestaurant(dto);
-        if (!msg.IsSuccessStatusCode)
+        if (msg.IsSuccessStatusCode)
         {
-            return BadRequest();
+            _notyf.Success("Created Restaurant!");
+            return RedirectToAction("Index");
         }
-
-        return RedirectToAction("Index");
+        _notyf.Error("Failed to create restaurant");
+        return BadRequest();
     }
 
     [HttpDelete]
@@ -59,6 +63,7 @@ public class RestaurantController : Controller
             return RedirectToAction("Index");
         }
         
+        _notyf.Success("Successfully deleted restaurant!");
         return RedirectToAction("Index");
     }
 }
--- RestaurantClient/Program.cs
+++ RestaurantClient/Program.cs
@@ -1,6 +1,9 @@
 using System.Net.Http.Headers;
+using AspNetCoreHero.ToastNotification;
+using AspNetCoreHero.ToastNotification.Extensions;
 using RestaurantClient.Interfaces;
 using RestaurantClient.Services;
+using Westwind.AspNetCore.LiveReload;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,8 +17,33 @@ builder.Services.AddHttpClient("RestaurantAPI",
             new MediaTypeWithQualityHeaderValue(
                 "application/json", 1.0));
     });
+builder.Services.AddLiveReload(config =>
+{
+    config.FileInclusionFilter = path =>
+    {
+        // don't trigger refreshes for files changes in the LocalizationAdmin sub-folder
+        if (path.Contains("\\LocalizationAdmin", StringComparison.OrdinalIgnoreCase))
+            return FileInclusionModes.DontRefresh;
+
+        // explicitly force a file to refresh without regard to file extension et al. rules
+        // are checked before the configuration file filter is applied
+        if (path.Contains("/customfile.mm"))
+            return FileInclusionModes.ForceRefresh;
+            
+        // continue regular file extension list filtering
+        return FileInclusionModes.ContinueProcessing;
+    };
+});
 builder.Services.AddScoped<IDishService, DishService>();
 builder.Services.AddScoped<IRestaurantService, RestaurantService>();
+builder.Services.AddNotyf(config =>
+{
+    config.DurationInSeconds = 5;
+    config.IsDismissable = true;
+    config.Position = NotyfPosition.BottomRight;
+});
+builder.Services.AddMvc().AddRazorRuntimeCompilation();
+builder.Services.AddMvc().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
@@ -28,8 +56,9 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseLiveReload();
+app.UseNotyf();
 app.UseStaticFiles();
-
 app.UseRouting();
 
 app.UseAuthorization();
